e9edeec4036e0ed413689eb4d6c632876ac6d7d5
Florin Dragos <florin@puppet.com>
2019-05-23 14:13:08 +0300

    Add docker experimental provisioner

---
+++ tasks/docker_exp.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'yaml'
+require 'puppet_litmus'
+
+# TODO: detect what shell to use
+@shell_command = 'bash -lc'
+
+def run_local_command(command)
+  stdout, stderr, status = Open3.capture3(command)
+  error_message = "Attempted to run\ncommand:'#{command}'\nstdout:#{stdout}\nstderr:#{stderr}"
+  raise error_message unless status.to_i.zero?
+  stdout
+end
+
+def provision(docker_platform, inventory_location)
+  include PuppetLitmus::InventoryManipulation
+  inventory_full_path = File.join(inventory_location, 'inventory.yaml')
+  inventory_hash = if File.file?(inventory_full_path)
+                     inventory_hash_from_inventory_file(inventory_full_path)
+                   else
+                     { 'groups' => [{ 'name' => 'docker_nodes', 'nodes' => [] }, { 'name' => 'ssh_nodes', 'nodes' => [] }, { 'name' => 'winrm_nodes', 'nodes' => [] }] }
+                   end
+
+  deb_family_systemd_volume = if (docker_platform =~ %r{debian|ubuntu}) && (docker_platform !~ %r{debian8|ubuntu14})
+                                '--volume /sys/fs/cgroup:/sys/fs/cgroup:ro'
+                              else
+                                ''
+                              end
+  creation_command = "docker run -d -it #{deb_family_systemd_volume} --privileged #{docker_platform}"
+  container_id = run_local_command(creation_command).strip
+  node = { 'name' => container_id,
+           'config' => { 'transport' => 'docker', 'docker' => { 'shell-command' => @shell_command } },
+           'facts' => { 'provisioner' => 'docker', 'container_id' => container_id, 'platform' => docker_platform } }
+  group_name = 'docker_nodes'
+  add_node_to_group(inventory_hash, node, group_name)
+  File.open(inventory_full_path, 'w') { |f| f.write inventory_hash.to_yaml }
+  { status: 'ok', node_name: container_id }
+end
+
+def tear_down(node_name, inventory_location)
+  include PuppetLitmus::InventoryManipulation
+  inventory_full_path = File.join(inventory_location, 'inventory.yaml')
+  raise "Unable to find '#{inventory_full_path}'" unless File.file?(inventory_full_path)
+  inventory_hash = inventory_hash_from_inventory_file(inventory_full_path)
+  node_facts = facts_from_node(inventory_hash, node_name)
+  remove_docker = "docker rm -f #{node_facts['container_id']}"
+  run_local_command(remove_docker)
+  remove_node(inventory_hash, node_name)
+  puts "Removed #{node_name}"
+  File.open(inventory_full_path, 'w') { |f| f.write inventory_hash.to_yaml }
+  { status: 'ok' }
+end
+
+params = JSON.parse(STDIN.read)
+platform = params['platform']
+action = params['action']
+node_name = params['node_name']
+inventory_location = params['inventory']
+raise 'specify a node_name if tearing down' if action == 'tear_down' && node_name.nil?
+raise 'specify a platform if provisioning' if action == 'provision' && platform.nil?
+
+begin
+  result = provision(platform, inventory_location) if action == 'provision'
+  result = tear_down(node_name, inventory_location) if action == 'tear_down'
+  puts result.to_json
+  exit 0
+rescue => e
+  puts({ _error: { kind: 'facter_task/failure', msg: e.message } }.to_json)
+  exit 1
+end
