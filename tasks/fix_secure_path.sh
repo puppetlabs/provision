94842b1e2cf2d22a451f6b4b1b8d64a4e0efe506
Thomas Krieger <tom@tom-krieger.de>
2020-04-02 18:08:33 +0200

    add pupet pagent bin directory to sudo secure_path setting

---
+++ tasks/fix_secure_path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "${PT_path}" ] ; then
+  pupt_pah="/opt/puppetlabs/bin"
+else
+  pupt_path=$PT_path
+fi
+
+sed -i -r -e "/^\s*Defaults\s*secure_path\s*/ s#=+\"([^\"]+)\".*#=\"\1:${pupt_path}\"#" /etc/sudoers
+sed -i -r -e "/^\s*Defaults\s+secure_path/ s#=([^\"].*)#=\1:${pupt_path}#" /etc/sudoers
+
+exit 0
